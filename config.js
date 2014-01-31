8cf41f345b9d5bdfe1673be0ca18a73f21d1a4b1
barroco <barroco@ebu.ch>
2014-01-31 17:48:31 +0100

    Handle when no rules apply

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ var config = {
   proxies:[
     {
       'local_port': 8080,
-      'host': 'www.ebu.io'
+      'host': 'www.ebu.io:5000'
     },
     {
       'local_port': 8081,
-      'host': 'bbc1.ebu.io'
+      'host': 'bbc1.ebu.io:5000'
     },
     {
       'local_port': 8082,
-      'host': 'bbc2.cpalocal.ebu.io'
+      'host': 'bbc2.cpalocal.ebu.io:5000'
     }
   ],
   error_message: 'The system is down.\n\n2014 - EBU Technology & Innovation - ebu.io'
--- proxy-server.js
+++ proxy-server.js
@@ -2,22 +2,35 @@ var http = require('http'),
     httpProxy = require('http-proxy'),
     config = require('./config');
 
-var proxy = httpProxy.createProxyServer({});
 
-proxy.on('error', function(err, req, res) {
+var sendError = function(err, req, res) {
   res.writeHead(500, {
     'Content-Type': 'text/plain'
   });
 
   res.end(config.error_message);
+};
+
+var proxy = httpProxy.createProxyServer({});
+
+proxy.on('error', function(err, req, res) {
+  sendError(err, req, res);
 });
 
 var server = require('http').createServer(function(req, res) {
+  var reqHost = req.headers['host'];
+
   for(c in config.proxies) {
-  	if(req.headers['host'] === config.proxies[c].host) {
-      proxy.web(req, res, { target: 'http://127.0.0.1:' + config.proxies[c].local_port });
+
+    var configHost = config.proxies[c].host;
+
+    if(reqHost === configHost){
+      return proxy.web(req, res, { target: 'http://127.0.0.1:' + config.proxies[c].local_port });
     }
   }
+
+  console.log('No rules for :', req.headers['host']);
+  sendError(new Error('No rule for host'), req, res);
 });
 
 console.log('listening on port ' + config.listening_port);
